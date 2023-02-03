32f991d3a01a383d7559588727b4856604e0d185
Juan Martin Castañar Petarcich <juanpetarcich@gmail.com>
2023-02-03 00:56:18 -0300

    Base de datos, se inicia ogin auth con JWT

---
+++ src/main/java/com/porfolio/juanargpr/JuanargprApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.juanargpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuanargprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuanargprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porfolio/juanargpr/JuanargprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.juanargpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanargprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
