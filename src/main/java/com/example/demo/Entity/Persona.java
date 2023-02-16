ee2987102e8a7e3677d16560442b5fd5d4e87d28
Juan Martin Castañar Petarcich <juanpetarcich@gmail.com>
2023-02-16 01:38:24 -0300

    Restauracion 2.6.7 con JDK 8

---
+++ src/main/java/com/example/demo/Entity/Persona.java
@@ -0,0 +1,13 @@
+
+package com.example.demo.Entity;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Grincode
+ */
+@Entity
+public class Persona {
+    
+}
---
+++ src/main/java/com/example/demo/PorfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorfolioApplication.class, args);
+	}
+
+}
