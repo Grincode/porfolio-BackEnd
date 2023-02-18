1fa7edc34cb7cddbace5c8d9fb3a4bceae80548f
Juan Martin Castañar Petarcich <juanpetarcich@gmail.com>
2023-02-18 19:06:38 -0300

    Experencia laboral con boton editar y borrar

---
+++ src/main/java/com/example/demo/Dto/dtoExperiencia.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ *
+ * @author Grincode
+ */
+public class dtoExperiencia {
+
+    @NotBlank
+    private String nombreE;
+    @NotBlank
+    private String descripcionE;
+
+    public dtoExperiencia() {
+    }
+
+    public dtoExperiencia(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
+
+}
+
+
--- src/main/java/com/example/demo/Repository/RExperiencia.java
+++ src/main/java/com/example/demo/Repository/RExperiencia.java
@@ -16,5 +16,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RExperiencia extends JpaRepository<Experiencia, Integer> {
     public Optional<Experiencia> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
     
 }
