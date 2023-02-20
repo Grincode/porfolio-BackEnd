79cea20eb1aa1411e8b5cd32cbaa5f4534b51fc9
Juan Martin Castañar Petarcich <juanpetarcich@gmail.com>
2023-02-20 15:10:44 -0300

    Ultimos cambios para deploy

--- src/main/java/com/example/demo/Repository/REducacion.java
+++ src/main/java/com/example/demo/Repository/REducacion.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.example.demo.Repository;
 
 import com.example.demo.Entity.Educacion;
