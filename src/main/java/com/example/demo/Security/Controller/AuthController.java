a00a1a2dec5ef209c8c93f1b2a9ac28e367e3350
Juan Martin Castañar Petarcich <juanpetarcich@gmail.com>
2023-02-24 16:03:19 -0300

    Ambiente de prueba y produccion en controller

--- src/main/java/com/example/demo/Security/Controller/AuthController.java
+++ src/main/java/com/example/demo/Security/Controller/AuthController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.example.demo.Security.Controller;
 
 import com.example.demo.Security.Dto.JwtDto;
